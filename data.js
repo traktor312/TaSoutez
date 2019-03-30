e776fe0997a2be3d519f9a018573e614b9679f02
it1701 <36028779+traktor312@users.noreply.github.com>
2019-03-30 19:50:12 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+var clanky = [
+    {
+        "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. A adipisci autem obcaecati cumque voluptates. Veniam error est possimus facere ipsum quia fuga harum, architecto, eaque, distinctio minima excepturi labore mollitia.",
+        "nadpis": "Lorem",
+        mesic: 5,
+        den: 3,
+    },
+    {
+        "text": "jgsdfjklsjkfldajkshdfljkasheklnscelruaialucgfgxefmlsaewiefowihpmcetzaerui gcmaiofxpoemsmxfmasepjyxf,oiasfeiophympasff,mshioerfpas",
+        "nadpis": "Random klavesy mackam",
+        mesic: 1,
+        den: 1,
+    },
+    {
+        "text": "13137591839491789657985984178529178507197204170160827640108576107839911531577336730637853104805360714738107482617634071863740826148027403186734036",
+        "nadpis": "Cisla",
+        mesic: 12,
+        den: 31,
+    },
+    {
+        "text": "Wiki je označení webů (nebo obecněji hypertextových dokumentů), které umožňují uživatelům přidávat obsah podobně jako v internetových diskusích, ale navíc jim také umožňují měnit stávající obsah; v přeneseném smyslu se jako wiki označuje software, který takovéto weby vytváří.",
+        "nadpis": "Wiki",
+        mesic: 7,
+        den: 24,
+    }
+]
---
+++ script.js
@@ -0,0 +1,93 @@
+// Pamatuje ktere tlacitko ma text skryt a ktere ukazat (0 = ukazat, 1 = schovat)
+var dalsi = [];
+// Indexy clanku po seradeni (na zacatku serazene podle data)
+var clanek = [];
+
+for (var i = 0; i < clanky.length; i++) {
+    dalsi.push(0);
+}
+
+function novyClanek(i) {
+    var output = "<div id='div" + i + "'><h1>";
+    output += clanky[i].den + '. ' + clanky[i].mesic + '. ' + clanky[i].nadpis;
+    output += "</h1><p>";
+    output += clanky[i].text;
+    //dalsi text
+    output += "</p><p class='hidden'>";
+    output += clanky[i].text;
+    //
+    output += '</p><button onclick="dale(' + i + ')">Číst dále...</button></div>';
+    return output;
+}
+
+function razeniNazev() {
+    var pole = [];
+    var pom = 0;
+    for (var i = 0; i < clanky.length; i++) {
+        pole.push(i);
+    }
+    for (var i = 0; i < clanky.length; i++) {
+        for (var j = 0; j < clanky.length - 1; j++) {
+            if (clanky[pole[j]].nadpis.charCodeAt(0) > clanky[pole[j + 1]].nadpis.charCodeAt(0)) {
+                pom = pole[j];
+                pole[j] = pole[j + 1];
+                pole[j + 1] = pom;
+            }
+        }
+    }
+    return pole;
+}
+
+function razeniDatum() {
+    var pole = [];
+    var pom = 0;
+    for (var i = 0; i < clanky.length; i++) {
+        pole.push(i);
+    }
+    for (var i = 0; i < clanky.length; i++) {
+        for (var j = 0; j < clanky.length - 1; j++) {
+            if (clanky[pole[j]].mesic + clanky[pole[j]].den * 0.01 > clanky[pole[j + 1]].mesic + clanky[pole[j + 1]].den * 0.01) {
+                pom = pole[j];
+                pole[j] = pole[j + 1];
+                pole[j + 1] = pom;
+            }
+        }
+    }
+    return pole;
+}
+clanek = razeniDatum();
+document.getElementById('pocet').value = 4;
+
+function vypisClanky() {
+    document.getElementById("body").innerHTML = "";
+    if (document.getElementById('razeni').value == 'vzestupne') {
+        for (var i = 0; i < document.getElementById('pocet').value; i++) {
+            document.getElementById("body").innerHTML += novyClanek(clanek[i]);
+        }
+    }
+    else {
+        for (var i = clanky.length - 1; i >= clanky.length - document.getElementById('pocet').value; i--) {
+            document.getElementById("body").innerHTML += novyClanek(clanek[i]);
+        }
+    }
+}
+vypisClanky();
+
+// Tlacitko cist dale
+function dale(i) {
+    var id = 'div' + i;
+    if (dalsi[i] == 1) {
+        document.getElementById(id).childNodes[2].style.display = 'none';
+        dalsi[i]--;
+    }
+    else {
+        document.getElementById(id).childNodes[2].style.display = 'block';
+        dalsi[i]++;
+    }
+}
+//
+
+document.getElementById('vyber').addEventListener('click', function () {
+    document.getElementById('podle').value == 'datum' ? clanek = razeniDatum() : clanek = razeniNazev();
+    vypisClanky();
+})
